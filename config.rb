504169459ab2fd59bf5bd211398bd90541295de0
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-10-14 00:28:13 +0200

    Compile stylesheet as compact

--- config.rb
+++ config.rb
@@ -3,6 +3,6 @@ relative_assets = true
 css_dir = "views/default/css/framework/wall"
 sass_dir = "sass"
 
-output_style = :expanded
+output_style = :compact
 line_comments = false
 preferred_syntax = :scss
