ed73c7b5071cefb5cc87095f61eb1fd46a3abeb0
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-11-25 09:23:56 +0300

    Fixes #46. Entity in vars is not a user

--- views/default/plugins/hypeWall/usersettings.php
+++ views/default/plugins/hypeWall/usersettings.php
@@ -3,6 +3,7 @@
 namespace hypeJunction\Wall;
 
 $entity = elgg_extract('entity', $vars);
+$user = elgg_get_page_owner_entity();
 
 echo '<div>';
 echo '<label>' . elgg_echo('wall:usersettings:river_access_id') . '</label>';
@@ -14,7 +15,7 @@ unset($user_write_access[ACCESS_LOGGED_IN]);
 
 echo elgg_view('input/access', array(
 	'name' => 'params[river_access_id]',
-	'value' => elgg_get_plugin_user_setting('river_access_id', $entity->guid, PLUGIN_ID),
+	'value' => elgg_get_plugin_user_setting('river_access_id', $user->guid, PLUGIN_ID),
 	'options_values' => $user_write_access,
 ));
 echo '</div>';
@@ -24,7 +25,7 @@ if (elgg_get_plugin_setting('third_party_wall', PLUGIN_ID)) {
 	echo '<label>' . elgg_echo('wall:usersettings:third_party_wall') . '</label>';
 	echo elgg_view('input/access', array(
 		'name' => 'params[third_party_wall]',
-		'value' => elgg_get_plugin_user_setting('third_party_wall', $entity->guid, PLUGIN_ID),
+		'value' => elgg_get_plugin_user_setting('third_party_wall', $user->guid, PLUGIN_ID),
 		'options_values' => array(
 			0 => elgg_echo('option:no'),
 			1 => elgg_echo('option:yes'),
