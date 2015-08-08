3e0d8399bd740d99d9edcdd119fca9c84432fd6d
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-08-08 16:21:40 +0200

    feature(grunt): automate releases

--- autoloader.php
+++ autoloader.php
@@ -4,13 +4,10 @@ if (!is_callable('hypeApps')) {
 	throw new Exception("hypeWall requires hypeApps");
 }
 
-$path = dirname(dirname(__DIR__));
-
-if (!file_exists("{$path}/vendor/autoload.php")) {
-	throw new Exception('hypeWall can not resolve composer dependencies. Run composer install');
+$path = __DIR__;
+if (file_exists("{$path}/vendor/autoload.php")) {
+	require_once "{$path}/vendor/autoload.php";
 }
-
-require_once "{$path}/vendor/autoload.php";
 
 /**
  * Plugin container
