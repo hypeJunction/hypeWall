a3530c2acff3d68866f030660d54a9c981b5e399
Javier M <javier.root@cloudaws.es>
2018-01-25 11:28:47 +0100

    Add es language file

---
+++ languages/es.php
@@ -0,0 +1,150 @@
+<?php
+
+return array(
+	'item:object:hjwall' => 'Publicaciones del muro',
+
+	'wall' => 'Muro',
+
+	'wall:settings:model:character_limit' => 'Limit the number of characters in a status message to (0 for no limit)',
+	'wall:settings:url' => 'Share a link',
+	'wall:settings:photo' => 'Share a photo',
+	'wall:settings:file' => 'Share a file',
+	'wall:settings:content' => 'Share content',
+	'wall:settings:geopositioning' => 'Enable geopositioning',
+	'wall:settings:tag_friends' => 'Enable tagging of friends',
+	'wall:settings:tags' => 'Enable word tags',
+	'wall:settings:third_party_wall' => 'Allow users to post on walls of other users that are not friends '
+	. '(each user will have to opt in to receive wall posts from non friends)',
+
+	'wall:usersettings:river_access_id' => 'Visibilidad de las etiquetas del muro',
+	'wall:usersettings:river_access_id:help' => '
+		¿Quién puede ver que te etiquetaron en el muro de otro usuario, si la publicación original no se compartió con ellos?
+	',
+
+	'wall:usersettings:third_party_wall' => 'Permitir que las personas que no son sus amigos publiquen en su muro',
+
+	'wall:write' => 'Publicar en el muro',
+	'wall:view' => 'Ver muro',
+
+	'wall:empty' => 'Este muro está vacío',
+	'wall:notfound' => 'La publicación no pudo ser encontrada',
+
+	'wall:tag:friends' => 'Etiquetar amigos',
+	'wall:tag:friends:hint' => 'Etiquetar amigos: comience la búsqueda escribiendo su nombre',
+	'wall:tag:location:hint' => 'Agrega un lugar o una ubicación a tu publicación',
+	'wall:tag:location:findme' => 'Encuéntrame - Tu navegador podría pedirte que permitas que este sitio use tu ubicación actual',
+	'wall:tag:river' => '%s etiquetado %s en %s',
+	'wall:tag:river:post' => 'publicación',
+
+	'wall:status:placeholder' => 'Muro: Publica tu anuncio',
+	'wall:url:placeholder' => 'Agregar un enlace',
+
+	'wall:tagged:post' => 'una publicación',
+	'wall:tagged:notification:subject' => '%s te etiquetó en %s',
+	'wall:tagged:notification:message' => '
+		%s te etiquetó en una publicación: <br />
+		<blockquote>
+			%s
+		</blockquote>
+		Puedes ver la publicación aquí:
+		%s
+	',
+
+	'wall:new:notification:generic' => 'Nueva publicación',
+	'wall:new:notification:summary' => 'Nueva publicación %s', // New post on X's wall
+	'wall:new:notification:subject' => '%s ha publicado %s', // X posted on Y's wall
+	'wall:new:notification:message' => '
+		%s ha publicado %s: <br />
+		<blockquote>
+			%s
+		</blockquote>
+		Puedes ver la publicación aquí:
+		%s
+	',
+
+	'wall:owner:suffix' => ' en el muro de %s',
+	'wall:byline' => ' por %s',
+	'wall:with' => '- con %s',
+	'wall:at' => ' cerca de %s',
+	'wall:attached:single' => '[1 adjunto]',
+	'wall:attached' => ' [%s adjuntos]',
+
+	'wall:new:wall:post' => '%s ha publicado en el muro de %s',
+	'wall:new:address' => 'ha compartido un enlace',
+	'wall:new:images' => 'ha publicado %s imágenes',
+	'wall:new:items' => 'ha publicado %s ítem',
+	'wall:new:attachments' => 'ha publicado %s imágenes y %s otros ítems',
+	'wall:new:status' => 'ha publicado',
+	'wall:new:onwall' => 'en el muro de %s',
+
+	'wall:status' => 'Actualizar estado',
+	'wall:url' => 'Compartir un enlace',
+	'wall:content' => 'Compartir contenido',
+	'wall:attachment' => 'Adjuntar contenido',
+	'wall:location' => 'Agregar ubicación',
+	'wall:tag_friends' => 'Etiquetar amigos',
+	'wall:upload_file' => 'Seleccionar un archivo',
+	'wall:find_me' => 'Encuéntrame',
+	'wall:post' => 'Publicar',
+	'wall:photo' => 'Compartir una foto',
+	'wall:file' => 'Compartir un archivo',
+	'wall:owner' => 'Muro de %s',
+	'wall:moreposts' => 'Más publicaciones',
+	'wall:filefolder' => 'Ficheros del muro',
+	'wall:upload' => 'Subida de ficheros en el muro',
+	'wall:tags' => 'Agregar etiquetas',
+	'wall:repost' => 'Publicar en tu muro (repost)',
+	'wall:photo:placeholder' => 'Decir algo acerca de esta foto',
+	'wall:file:placeholder' => 'Cuéntanos algo sobre este archivo',
+	'wall:filehasntuploaded' => 'Espere a que se cargue el archivo',
+
+	'wall:create:success' => 'La publicación en el muro se guardó con éxito',
+	'wall:create:error' => 'La publicación en el muro no se pudo crear',
+	'wall:process:posting' => 'Publicando ...',
+
+	'wall:error:ajax' => 'No se puede acceder a la página remota',
+	'wall:error:not_found' => 'Publicación no encontrada',
+	'wall:error:container_permissions' => 'Usted no tiene suficientes permisos para publicar aquí',
+	'wall:error:empty_form' => 'Cuéntanos qué tienes en mente o agrega primero un enlace',
+
+	'wall:edit' => 'Editar publicación',
+
+	'wall:delete' => 'Eliminar publicación en el muro',
+	'wall:delete:success' => 'Publicación borrada con éxito',
+	'wall:delete:error' => 'La publicación en el muro no se pudo eliminar',
+
+	'wall:remove_tag' => 'Eliminar etiqueta',
+	'wall:remove_tag:success' => 'Ya no está etiquetado en esta publicación',
+	'wall:remove_tag:error' => 'La etiqueta no se pudo eliminar',
+
+	'wall:post:status_update' => 'Actualización de estado %s',
+	'wall:post:wall_to_wall' => 'Publicación del muro %s',
+
+	'wall:ecml:url' => 'Dirección URL del muro',
+	'wall:ecml:attachment' => 'Adjunto',
+	'wall:ecml:river' => 'Vista del timeline del muro',
+
+	'wall:upload:success' => 'Archivo cargado con éxito',
+	'wall:upload:error' => 'El archivo no se pudo cargar',
+
+	'wall:characters_remaining' => "caracteres restantes",
+	'wall:make_bookmark' => 'Guardar este enlace en mis marcadores',
+
+	'wall:numbertodisplay' => 'Número de publicaciones más recientes a mostrar',
+
+	'wall:target:hjwall' => 'Muro',
+
+	'wall:ownership:own' => 'en su %s',
+	'wall:ownership:your' => 'en tu %s',
+	'wall:ownership:owner' => '%s en su %s',
+
+	'wall:widget:description' => 'Permite que otros vean y publiquen en tu muro',
+	'wall:widget:showaddform' => 'Mostrar un formulario para agregar nuevas publicaciones',
+
+	'wall:groups:enable' => 'Habilitar muro de grupo',
+	'wall:groups' => 'Muro del grupo',
+	'wall:groups:post' => 'Publicar',
+
+	'hypeWall:usersettings:title' => 'Muro',
+);
+
